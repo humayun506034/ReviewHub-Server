c46e6dd01fc23743e3afb471dc33e928b4a20f8a
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-02-24 14:46:02 +0600

    chore: stop tracking dist folder

--- dist/app/constants/searchableFieldConstant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserSearchableFields = exports.ReviewSearchableFields = void 0;
-exports.ReviewSearchableFields = [
-    "title",
-    "description",
-    "excerp",
-];
-exports.UserSearchableFields = ["name", "email"];
